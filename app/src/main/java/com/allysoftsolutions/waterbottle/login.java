dfd77f1ad77fb50ab3987de83c6551164cf10956
saarzooms <arzoo.rkcet@gmail.com>
2019-01-19 12:42:26 +0530

    anil1

---
+++ app/src/main/java/com/allysoftsolutions/waterbottle/login.java
@@ -0,0 +1,22 @@
+package com.allysoftsolutions.waterbottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+    }
+
+    public void logincancel(View view) {
+    }
+
+    public void login(View view) {
+
+
+    }
+}
---
+++ app/src/main/java/com/allysoftsolutions/waterbottle/register.java
@@ -0,0 +1,23 @@
+package com.allysoftsolutions.waterbottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_register );
+    }
+
+    public void cancel(View view) {
+    }
+
+    public void register(View view) {
+    }
+
+    public void addimage(View view) {
+    }
+}
