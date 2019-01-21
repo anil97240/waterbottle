e20f3eeb43f2f9cba31a61be2f2cad72c9443663
saarzooms <arzoo.rkcet@gmail.com>
2019-01-21 13:13:07 +0530

    a1

--- app/src/main/java/com/allysoftsolutions/waterbottle/MainActivity.java
+++ app/src/main/java/com/allysoftsolutions/waterbottle/MainActivity.java
@@ -2,22 +2,32 @@ package com.allysoftsolutions.waterbottle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.allysoftsolutions.waterbottle.validations.Validation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btSignIn;
-    private Button btSignUp;
-    private EditText edtEmail;
-    private EditText edtPassword;
+     Button btSignIn;
+     Button btSignUp;
+     EditText edtEmail;
+     EditText edtPassword;
     RelativeLayout rv;
+
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.emailinput);
         edtPassword = findViewById(R.id.passwordinput);
 
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, UserActivity.class));
+            finish();
+        }
 
 
         btSignUp.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +62,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (Validation.isValidEmail(edtEmail.getText(), edtPassword.getText())) {
 
+                    String email = edtEmail.getText().toString();
+                    String password = edtPassword.getText().toString();
+
+
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    //progressBar.setVisibility(View.GONE);
+                                    if (!task.isSuccessful()) {
+                                        // there was an error
+                                      //  if (password.length() < 6) {
+                                            //inputPassword.setError(getString(R.string.minimum_password));
+                                    //    } else {
+                                            //Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    //    }
+                                    } else {
+                                        Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+
+
+
                 } else {
                     Toast.makeText(MainActivity.this, "Enter kro Kaik", Toast.LENGTH_SHORT).show();
 
@@ -55,6 +101,4 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
 }
