b80fce82b486261226ee1a8ebf02d0e0a76d4754
saarzooms <arzoo.rkcet@gmail.com>
2019-01-22 16:56:09 +0530

    a1

--- app/src/main/java/com/allysoftsolutions/waterbottle/AgentDashboard.java
+++ app/src/main/java/com/allysoftsolutions/waterbottle/AgentDashboard.java
@@ -2,12 +2,20 @@ package com.allysoftsolutions.waterbottle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AgentDashboard extends AppCompatActivity {
 
     TextView tv;
 
+    EditText number;
+    Button b1,b2;
+
+int a=1,b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,15 +23,47 @@ public class AgentDashboard extends AppCompatActivity {
         tv = findViewById(R.id.txtqrs);
         setTitle("Agent Dashboard");
 
+        b1=findViewById(R.id.button);
+        b2=findViewById(R.id.button2);
+        number=findViewById(R.id.editText2);
 
-        Bundle extras = getIntent().getExtras();
+       /* Bundle extras = getIntent().getExtras();
         String userName = null;
 
         if (extras != null) {
             userName = extras.getString("qrcode");
             // and get whatever type user account id is
         }
-        tv.setText(userName.toString());
+//        tv.setText(userName.toString());*/
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // b= Integer.parseInt(number.getText().toString());
+                //int b = 0;
+            b=b+a;
+
+               number.setText(""+b);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             //   b= Integer.parseInt(number.getText().toString());
+
+                b=b-a;
+                number.setText(""+b);
+            }
+        });
+
+
+
 
     }
+
+    public void done(View view) {
+
+        Toast.makeText(this, "You delivered ..."+ number +"Bottle", Toast.LENGTH_SHORT).show();
+    }
+
 }
